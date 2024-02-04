8137cf4f7c6add144886bd161c47e5d036c2bf7d
Coreym6 <cmcdani3@samford.edu>
2024-02-03 23:16:44 -0500

    generic examples

--- Study Implementations/AA.py
+++ Study Implementations/AA.py
@@ -1,12 +1,22 @@
 # Some of the implementation of things that I've learned in the Advanced Algorithms course & Data mining course
 # insertionsort, mergesort, quicksort, heapsort, binarysearch, linearsearch and selection sort
 
-# insertion_sort
+# example of an insertion_sort
+def insertion_sort(arr):
+    for i in range(1, len(arr)):
+        key = arr[i]
+        j = i-1
+        while j >= 0 and key < arr[j]:
+            arr[j+1] = arr[j]
+            j -= 1
+        arr[j+1] = key
+    return arr
 
 
 
-# merge_sort 
+#example of a merge_sort 
 
+testM_array = [12, 13, 14, 78, 65, 90, 120]
 
 # quick_sort
 
